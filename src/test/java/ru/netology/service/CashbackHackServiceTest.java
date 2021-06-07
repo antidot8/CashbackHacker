16a134fc2756cf7dea64a9d56d40de4d04fdbea0
Alexander Siluyanov <antidot8@mail.ru>
2021-06-07 19:07:36 +0400

    Tests with JUnit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,28 +9,24 @@ public class CashbackHackServiceTest {
     @Test
     public void calculateIfAmountLess1000() {
         int amount = 800;
-        int expected = cashbackHackService.remain(amount);
-        assertEquals(200, expected);
+        assertEquals(200, cashbackHackService.remain(amount));
     }
 
     @Test
     public void calculateIfAmountMore1000() {
         int amount = 1100;
-        int expected = cashbackHackService.remain(amount);
-        assertEquals(900, expected);
+        assertEquals(900, cashbackHackService.remain(amount));
     }
 
     @Test //этот тест падает
     public void calculateIfAmountIs1000() {
         int amount = 1000;
-        int expected = cashbackHackService.remain(amount);
-        assertEquals(0, expected);
+        assertEquals(0, cashbackHackService.remain(amount));
     }
 
     @Test
     public void calculateIfAmountIs0() {
         int amount = 0;
-        int expected = cashbackHackService.remain(amount);
-        assertEquals(1000, expected);
+        assertEquals(1000, cashbackHackService.remain(amount));
     }
 }
